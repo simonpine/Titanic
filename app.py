bf178c720c493c5fa3f4be72781353e513c464f5
simon pineda <simonpineda0521@gmail.com>
2023-12-11 23:22:06 -0500

    The model generation is finished

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pickle
+
+st.write('''
+# Titanic Prediction
+         
+This app predicts if a person could survive the Titanic with some information.
+         
+''')
